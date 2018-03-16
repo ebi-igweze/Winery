1e75dd9146c0e6a16fa5607974d011a416efdc5d
ebi igweze <ebi.igweze@gmail.com>
2018-03-17 00:07:01 +0100

    house-keeping for actors

--- src/Winery/Infrastructure/Services/StorageActors.fs
+++ src/Winery/Infrastructure/Services/StorageActors.fs
@@ -6,46 +6,28 @@ open Services.Models
 open Services.Actors.User
 
 let categoryActor (executioners: CategoryCommandExecutioners) (mailbox: Actor<_>) = 
-    let rec handleResult (envelope: Envelope<_>) = function 
-        | Ok message ->
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-        | Error message -> 
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-
-    and loop () = actor {
+    let eventStream = mailbox.Context.System.EventStream
+    let rec loop () = actor {
         let! envelope = mailbox.Receive ()
-        printfn "category actor received %O" envelope
-        do publish (commandEnvelopeReveived envelope) mailbox.Context.System.EventStream
+        do publish (commandEnvelopeReveived envelope) eventStream
+        let handleResult =  handleResult eventStream envelope.id
         match envelope.message with
-        | DeleteCategory (categoryId)               -> return! handleResult envelope <| executioners.deleteCategory categoryId
-        | AddCategory (categoryId, newCategory)     -> return! handleResult envelope <| executioners.addCategory (categoryId, newCategory)            
-        | UpdateCategory (categoryId, editCategory) -> return! handleResult envelope <| executioners.updateCategory (categoryId, editCategory)  
+        | UpdateCategory (categoryId, editCategory) -> return! loop << handleResult <| executioners.updateCategory (categoryId, editCategory)  
+        | AddCategory (categoryId, newCategory)     -> return! loop << handleResult <| executioners.addCategory (categoryId, newCategory)            
+        | DeleteCategory (categoryId)               -> return! loop << handleResult <| executioners.deleteCategory categoryId
     }
     loop ()   
 
 let wineActor (executioners: WineCommandExecutioners) (mailbox: Actor<_>) =
-    let rec handleResult (envelope: Envelope<_>) = function
-        | Ok message ->
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-        | Error message -> 
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-
-    and loop () = actor {
+    let eventStream = mailbox.Context.System.EventStream
+    let rec loop () = actor {
         let! envelope = mailbox.Receive ()
-
-        do publishEvent mailbox <| (commandEnvelopeReveived envelope)
+        do publish (commandEnvelopeReveived envelope) eventStream
+        let handleResult = handleResult eventStream envelope.id
         match envelope.message with 
-        | DeleteWine wineId                     -> return! handleResult envelope <| executioners.deleteWine wineId
-        | UpdateWine (wineId, editWine)         -> return! handleResult envelope <| executioners.updateWine (wineId, editWine)
-        | AddWine (categoryId, wineId, newWine) -> return! handleResult envelope <| executioners.addWine (categoryId, wineId, newWine)
+        | DeleteWine wineId                     -> return! loop << handleResult <| executioners.deleteWine wineId
+        | UpdateWine (wineId, editWine)         -> return! loop << handleResult <| executioners.updateWine (wineId, editWine)
+        | AddWine (categoryId, wineId, newWine) -> return! loop << handleResult <| executioners.addWine (categoryId, wineId, newWine)
     }
     loop ()
 
--- src/Winery/Infrastructure/Services/UserActors.fs
+++ src/Winery/Infrastructure/Services/UserActors.fs
@@ -1,15 +1,15 @@
 module Services.Actors.User
 
 open Winery
-open System
 open Akka.FSharp
 open Storage.Models
 open Services.Models
 open System.Collections.Generic
 
-let publishEvent (mailbox: Actor<_>) event = do mailbox.Context.System.EventStream.Publish event
-let subscribeEvent (mailbox: Actor<_>) event = mailbox.Context.System.EventStream.Subscribe event
 
+let handleResult eventStream commandId = function 
+    | Ok message -> publish (CommandCompleted (CommandID commandId, Message message, Success)) eventStream
+    | Error message -> publish (CommandCompleted (CommandID commandId, Message message, Failure)) eventStream
 
 type UserCommand =
 | AddUser of UserID * NewUser * Password
@@ -17,45 +17,26 @@ type UserCommand =
 | AddUserRole of UserID * UserRole
 
 let userActor (executioners: UserCommandExecutioners) (mailbox: Actor<_>)  =
-    let rec handleResult (envelope: Envelope<_>) = function
-        | Ok message -> 
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-
-        | Error message ->
-            do (CommandCompleted (CommandID envelope.id, Message message, Failure))
-               |> publishEvent mailbox
-            loop ()
-
-    and loop () = actor {
+    let eventStream = mailbox.Context.System.EventStream
+    let rec loop () = actor {
         let! envelope = mailbox.Receive ()
-        do publishEvent mailbox (commandEnvelopeReveived envelope)
+        do publish (commandEnvelopeReveived envelope) eventStream
+        let handleResult = handleResult eventStream envelope.id
         match envelope.message with
-        | AddUser (userId, newUser, password) -> return! handleResult envelope <| executioners.addUser (userId, newUser, password)
-        | UpdateUser (userId, editUser) -> return! handleResult envelope <| executioners.updateUser (userId, editUser)
-        | AddUserRole _ -> return! loop ()
+        | AddUser (userId, newUser, password) -> return! loop << handleResult <| executioners.addUser (userId, newUser, password)
+        | UpdateUser (userId, editUser)       -> return! loop << handleResult <| executioners.updateUser (userId, editUser)
+        | AddUserRole _                       -> return! loop ()
     }
-
     loop ()
 
 let cartActor (executioner: CartCommandExecutioner) (mailbox: Actor<_>) =
-    let rec handleResult (envelope: Envelope<_>) = function
-        | Ok message -> 
-            do (CommandCompleted (CommandID envelope.id, Message message, Success))
-               |> publishEvent mailbox
-            loop ()
-        | Error message ->
-            do (CommandCompleted (CommandID envelope.id, Message message, Failure))
-               |> publishEvent mailbox
-            loop ()
-
-    and loop () = actor {
+    let eventStream = mailbox.Context.System.EventStream
+    let rec loop () = actor {
         let! envelope = mailbox.Receive ()
-        do publishEvent mailbox <| (commandEnvelopeReveived envelope)
-        return! handleResult envelope <| (executioner envelope.message)
+        do publish (commandEnvelopeReveived envelope) eventStream 
+        let handleResult = handleResult eventStream envelope.id
+        return!  loop << handleResult <| (executioner envelope.message)
     }
-
     loop ()
 
 let commandActor (mailbox: Actor<_>) = 
@@ -69,42 +50,36 @@ let commandActor (mailbox: Actor<_>) =
         Seq.tryFind (fun (v : KeyValuePair<CommandID,_>) -> v.Key = commandId) 
         >> Option.map (fun keyValue -> keyValue.Value)
 
-    let rec handleResult (store: IDictionary<CommandID, Command>) = function 
-        | CommandReceived command -> 
-            printfn ("received command")
-            do KeyValuePair(command.id, (command)) |> store.Add
-            loop store
 
-        | CommandCompleted (commandId, Message message, status) -> 
-            printfn ("command completed")
-            store
-            |> tryGetCommand commandId
-            |> function
-                | Some command -> 
-                    command.resultStatus <- status
-                    command.resultMessage <- message
-                    store.[commandId] <- command
-                | None -> ()
-            |> ignore
-            loop store
-
-        | CommandStatusRequest commandId -> 
-            printfn ("command requested")
-            let command = tryGetCommand commandId <| store
-            let commandResult = 
-                command 
-                |> Option.map (fun command -> 
-                      let (CommandID id) = command.id
-                      { CommandResult.id = id
-                        message = command.resultMessage; 
-                        result = command.resultStatus; })
-
-            do mailbox.Sender() <! commandResult
-            loop store
-
-    and loop store = actor {
+    let rec loop (store: Dictionary<CommandID, Command>) = actor {
         let! message = mailbox.Receive ()
-        return! handleResult store <| message 
+        return! match message with 
+                | CommandReceived command -> 
+                    store.Add <| (command.id, command)
+                    loop store 
+
+                | CommandCompleted (commandId, Message message, status) -> 
+                    store
+                    |> tryGetCommand commandId
+                    |> function
+                        | None         -> ()
+                        | Some command -> 
+                            command.resultStatus <- status
+                            command.resultMessage <- message
+                            store.[commandId] <- command
+                    |> ignore
+                    loop store
+
+                | CommandStatusRequest commandId -> 
+                    let command = tryGetCommand commandId <| store
+                    let commandResult = command |> Option.map (fun command -> 
+                        let (CommandID id) = command.id
+                        { CommandResult.id = id
+                          message = command.resultMessage; 
+                          result = command.resultStatus; })
+
+                    do mailbox.Sender() <! commandResult
+                    loop store
     }
 
     loop (Dictionary())
